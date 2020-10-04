524795d4f64df8ab74b73ed457cdc313f0f59c28
Kennedy-karuri <kennkaruri99@gmail.com>
2020-10-05 02:11:30 +0300

    add index.html

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 var akanNameMale = ["Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"];
 var akanNameFemale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 
-function ghana() {
-    var centuary = parseInt(document.getElementById("centuary").value);
+function akanName() {
+    var century = parseInt(document.getElementById("century").value);
     var year = parseInt(document.getElementById("year").value);
     var month = parseInt(document.getElementById("month").value);
     var day = parseInt(document.getElementById("day").value);
     var gender = document.getElementById("gender").value;
-    var CC = centuary;
+    var CC = century;
     var YY = year;
     var MM = month;
     var DD = day;
     var d = parseInt(((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7
     if (CC < 18 || CC > 20) {
-        alert("Your centuary is invalid")
-    } else if (YY < 0 || 20 < YY) {
+        alert("Your century is invalid")
+    } else if (YY < 0 || 100 < YY) {
         alert("Invalid Year")
     } else if (MM <= 0 || MM > 12) {
         alert("Enter a valid Month")
